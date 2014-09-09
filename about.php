e842c7cd96593831cb757a93494bf9f69b8f2edb
Kenneth Joel <kennyjoelruiz@gmail.com>
2014-09-08 20:42:42 -0500

    Updated the About page to contain actual information rather than place-holder text.

--- about.php
+++ about.php
@@ -3,18 +3,21 @@
 	$sectionTitle = "about"; 
 	include('inc/header.php');
 ?>
-			<h1>Contact</h1>
-			<div class="contact-wrap">
-				<h3 class="pull-up">Email</h3> 
-				<div class="contact-info">kennyjoelruiz@gmail.com</div>
-				<h3 class="pull-up">LinkedIn</h3>
-				<div class="contact-info"><a href="http://www.linkedin.com/in/kennethjoelruiz">linkedinprofile.com/in/kennethjoelruiz</a></div>
-				<h3 class="pull-up">GitHub</h3>
-				<div class="contact-info"><a href="https://github.com/kennyjoel">https://github.com/kennyjoel</a></div>
-			</div> 
-
 			<h1>About</h1> 
-			<p>As you might know by now, my name is Kenneth Joel Ruiz. I'm from a small(ish) town in Texas called Cibolo, but I'm fortunate enough that I've been able to call a handful of places home. I was born in Ft. Benning, Georgia in 1993, but spent my childhood in Arecibo, Puerto Rico. I lived in Puerto Rico for ten years until I moved to the San Antonio area here in Texas. Since living here, I've graduated high school and attended Texas State University-San Marcos for two and a half years. Outside of college, I studied Linux system administration at the Rackspace Open Cloud Academy, and web development as a hobbyist at home.</p>
+			<p>As you might know by now, my name is Kenneth Joel Ruiz. <span class="ti-face-smile"></span></p>
+			<p>Cibolo, Texas is the town I would tell you I'm from if you asked, but I've been around a few places. Aside from Texas, I've lived in Puerto Rico for 10 years when I was a youngling. But for the last 10 years and counting, Texas has been home for me. </p>
+			<p>While living here, I studied Computer Science (with a minor in Math!) at Texas State University-San Marcos. And during the summer of 2014 I enrolled in a program outside of school called the Rackspace Open Cloud Academy. At the Open Cloud Academy, I studied Linux system administration.</p>
+			<p>When I'm not focusing on web development or other miscellaneous studies, I'm tending to my two fantastic kitties, Lucius and Isaac McFinnigan!</p>
+			<div class="avatar-container">
+				<img class="alt-avatar" src="img/lucius.jpg">
+				<img class="alt-avatar" src="img/isaac.jpg">
+			</div>
+			<p>Lucius is a cat that was rescued from the parking lot of a Bucc-ee's travel center when he was just a kitten. Lucius' hobbies include licking blinds, sitting in awkward positions, and chasing flies that sneak into the apartment. Isaac McFinnigan was a cat we took in from a previous owner who could no longer keep him; he was 6 years old when we brough him in. Isaac doesn't have time for a lot of hobbies because he requires 11.9 hours of sleep, and 11.3 hours of being petted. The remaining time is spent doing a combination of both.</p>
+
+			<p>Bye Bye!</p>
+			<div class="avatar-container">
+				<img class="alt-avatar" src="img/mclucius">
+			</div>
 		</div>
 	</div>
 </body>
