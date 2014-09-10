e4c03ea4c67de1357913fde609914facd6bf7c02
Kenneth Joel <kennyjoelruiz@gmail.com>
2014-09-10 12:25:15 -0500

    Added more content to the About page.

--- about.php
+++ about.php
@@ -5,16 +5,27 @@
 ?>
 			<h1>About</h1> 
 			<p>As you might know by now, my name is Kenneth Joel Ruiz. <span class="ti-face-smile"></span></p>
+
 			<p>Cibolo, Texas is the town I would tell you I'm from if you asked, but I've been around a few places. Aside from Texas, I've lived in Puerto Rico for 10 years when I was a youngling. But for the last 10 years and counting, Texas has been home for me. </p>
-			<p>While living here, I studied Computer Science (with a minor in Math!) at Texas State University-San Marcos. And during the summer of 2014 I enrolled in a program outside of school called the Rackspace Open Cloud Academy. At the Open Cloud Academy, I studied Linux system administration.</p>
+
+			<p>While living here, I've studied Computer Science (with a minor in Math!) at Texas State University-San Marcos. The time I spent at Texas State was humbling, and I met a lot of talented and passionate people who would end up inspiring me to do the things I'm doing today. While at Texas State, I was a member of an organization called ECO (Environmental Conservation Organization) which volunteered a lot of its time towards working on various environmental projects around San Marcos. I also had a brief stint as a radio host for the college radio station, KTSW 89.9.</p>
+
+			<p>During the summer of 2014 I enrolled in an 8-week program outside of school called the Rackspace Open Cloud Academy. At the Open Cloud Academy my studies focused on Linux system administration, but this program was largely responsible for laying the foundation for the majority of my technical knowledge. I learned a lot of fundamental software development skills in college, but I had never configured a web or database server until getting to the Open Cloud Academy.</p>
+
 			<p>When I'm not focusing on web development or other miscellaneous studies, I'm tending to my two fantastic kitties, Lucius and Isaac McFinnigan!</p>
 			<div class="avatar-container">
+				Lucius!<span class="ti-arrow-right"></span>
 				<img class="alt-avatar" src="img/lucius.jpg">
 				<img class="alt-avatar" src="img/isaac.jpg">
+				<span class="ti-arrow-left"></span>Isaac!
 			</div>
 			<p>Lucius is a cat that was rescued from the parking lot of a Bucc-ee's travel center when he was just a kitten. Lucius' hobbies include licking blinds, sitting in awkward positions, and chasing flies that sneak into the apartment. Isaac McFinnigan was a cat we took in from a previous owner who could no longer keep him; he was 6 years old when we brough him in. Isaac doesn't have time for a lot of hobbies because he requires 11.9 hours of sleep, and 11.3 hours of being petted. The remaining time is spent doing a combination of both.</p>
 
-			<p>Bye Bye!</p>
+			<p>I've been known to be a collector of hobbies, so the things I spend time doing tend to change from month to month. My most recent interest has been tabletop games. I've been an avid gamer since I could hold a controller, but as I've gotten older it's become difficult to spend more than a couple hours on a single video game. I've found tabletop games to be a great solution for finding a reason to sit in front of a group of people and socialize, have a game or two, but not have it eat up the whole evening if I have other responsibilities that night.</p>
+
+			<p>Overall, I just enjoy learning things. And I don't mean that in a cliche, job-interview-answer kind of way. It's just something I like to do. There's a lot of things I'm good at, and those are the things that I know a lot about. But I'm also ignorant about a lot, and I think that's great. It's great because I have a lot of life left to live and I need to do things to fill all that time, and as long as there's something left for me to learn then there's soemthing left for me to do.</p>
+
+			<p>And with that being said, the monologue comes to an end here. I hope you had a decent time reading a little about me, check out the blog for more! Farewell.</p>
 			<div class="avatar-container">
 				<img class="alt-avatar" src="img/mclucius">
 			</div>
