172e9b496d799a5c2162d8e9d062bf41f24ffd68
kennyjoel <kennyjoelruiz@gmail.com>
2014-09-16 16:43:38 -0700

    Implemented a "Read More..." hyperlink to the blog posts on the Blog page.

--- index.php
+++ index.php
@@ -13,21 +13,12 @@
 			involved creating a web server for a customer, and configuring it to work with Varnish. Now, I'm 
 			familiar with Varnish, and I understand what it does, but I've never installed and configured it
 			before. Alright, no problem. I open my search engine of choice, and start digging in.</p>
-			<p>When I have to implement a solution I haven't used before, I do a few things. First, 
-			I find the general documentation for solution, in this case, Varnish. Next, I attempt to gain a
-			surface level understanding of what the solution I'm using actually does. I feel like
-			I understand what Varnish does well enough to begin working on the ticket, but I poke around 
-			online to make sure I'm not mistaken. This saves me from backpedaling later if I'm wrong, and it 
-			doesn't take long to do; worst case being that I found out what I already knew. The last thing
-			I do is open up a guide or resource independent of the official documentation that helps
-			me do what I need to do. This is useful since documentation doesn't tend to outline common pitfalls
-			you can run into while working.</p>
-			<p>A problem I tend to run into when working on certain issues is having what I consider to be
-			documentation tunnel vision.
-			 </p>
+			<p>When I have to implement a solution I haven't used before, I do a few things.</p>
+			<a href="first_post.php">Read more...</a>
 
 			<h2> Ipsum the things, man.</h2> 
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+			<a href="second_post.php">Read more...</a>
 		</div> 
 	</div>
 </body>
