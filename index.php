8cc1faf72cf38389b6ca6d21277a00061b988218
kennyjoel <kennyjoelruiz@gmail.com>
2014-09-16 15:59:30 -0700

    Added the first blog post to index.php.

--- index.php
+++ index.php
@@ -5,8 +5,27 @@
 ?>
 			<h1 class="page-title pull-up-more">Hello, I'm Kenneth <span class="off-color">Joel</span>.</h1>
 			<p class="page-title">I do things with the internet and stuff.</p>
-			<h2>Lorem ipsum, dude.</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<h2 class="pull-up">Dude, that wasn't in documentation.</h2>
+			<div class="resume-date">By: Kenneth Ruiz | September 16th, 2014</div>
+			<p>The last week at Open Cloud is called "System Administration" week, and students are 
+			placed into a faux-ticket-queue and given practice tickets to work. The tickets can range from 
+			configuring a mail server to setting up virtual hosts for a web server. The ticket I worked on
+			involved creating a web server for a customer, and configuring it to work with Varnish. Now, I'm 
+			familiar with Varnish, and I understand what it does, but I've never installed and configured it
+			before. Alright, no problem. I open my search engine of choice, and start digging in.</p>
+			<p>When I have to implement a solution I haven't used before, I do a few things. First, 
+			I find the general documentation for solution, in this case, Varnish. Next, I attempt to gain a
+			surface level understanding of what the solution I'm using actually does. I feel like
+			I understand what Varnish does well enough to begin working on the ticket, but I poke around 
+			online to make sure I'm not mistaken. This saves me from backpedaling later if I'm wrong, and it 
+			doesn't take long to do; worst case being that I found out what I already knew. The last thing
+			I do is open up a guide or resource independent of the official documentation that helps
+			me do what I need to do. This is useful since documentation doesn't tend to outline common pitfalls
+			you can run into while working.</p>
+			<p>A problem I tend to run into when working on certain issues is having what I consider to be
+			documentation tunnel vision.
+			 </p>
+
 			<h2> Ipsum the things, man.</h2> 
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 		</div> 
