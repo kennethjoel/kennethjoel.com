eea64b7eaec94572b835974c914c09c192ce52d2
Kenneth Joel <kennyjoelruiz@gmail.com>
2014-09-17 22:55:49 -0500

    Removed the footer from all webpages.

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 
 <footer>
-	<span class="ti-linkedin footer-icon"></span>
-	<span class="ti-github footer-icon"></span>
+<!-- 	<span class="ti-linkedin footer-icon"></span>
+	<span class="ti-github footer-icon"></span> -->
 </footer>
